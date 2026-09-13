6c4214085bbc6542d20a791055099d4c2f03e480
PavelMetelev <114985000+PavelMetelev@users.noreply.github.com>
2026-09-14 02:46:39 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import type { ReactNode } from "react";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Arena Next.js PostgreSQL Starter",
-  description: "Starter template with Next.js, Drizzle, and PostgreSQL.",
+  title: "AdminsTools",
+  description: "Система выдачи наказания",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
